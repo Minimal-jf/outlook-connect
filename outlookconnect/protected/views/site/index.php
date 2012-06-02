fb91b7c1cf319ef051275c2e9b0c60891cfcaecd
Minimal-jf <jamesfidler@jf-pc>
2012-06-02 21:37:17 +0100

    Change to look like attempted layout. Redirect to login screen if not logged in.

--- outlookconnect/protected/views/site/index.php
+++ outlookconnect/protected/views/site/index.php
@@ -1,5 +1,17 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
+<div id="mainmenu">
+		<?php $this->widget('zii.widgets.CMenu',array(
+			'items'=>array(
+				array('label'=>'Home', 'url'=>array('/site/index')),
+				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+			),
+		)); ?>
+</div>
+
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
 <p>Congratulations! You have successfully created your Yii application.</p>
